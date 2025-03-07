2dcdeb960388ef135debfed1bb797f110ede08ce
edupoly <info.edupoly@gmail.com>
2025-03-07 10:01:50 +0530

    zxxc

--- socketserver.js
+++ socketserver.js
@@ -3,12 +3,17 @@ var app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 app.use(express.static(__dirname+"/public"))
-
+var c=0;
 io.on('connection', (socket) => {
-    socket.emit("greet","Hello ALL")
-    socket.on("newmsg",(x)=>{console.log(x)})
-    socket.on("connect",()=>{console.log(socket.id+"::connected");})
-    socket.on("disconnect",()=>{console.log(socket.id+"::disconnected");})
+    c++;
+    console.log("connection made",c)
+    io.emit("viewerscount",{count:c})
+    socket.on("disconnect",()=>{
+        c--;
+        io.emit("viewerscount",{count:c})
+        console.log("connection lost",c)
+    })
+    
 });
 
 server.listen(4000);
